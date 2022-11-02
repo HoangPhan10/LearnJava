d413a63e1e922285aa4d7fab5778262a1629abad
hoangphan10 <phanhoang1022002@gmail.com>
2022-11-02 19:28:51 +0700

    check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import com.company.connectionJDBC.ConnectionJDBC;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -18,12 +19,30 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        insertTable(5,"Nguyen Chi Quang");
-    }
+//        insertTable(5,"Nguyen Chi Quang");
+        updateTable(5,"Phan van Hoang");
+//        selectTable();
+          }
 
+
+    public static void updateTable(int id,String name){
+        String sql = "update lop set name = ? where ID = ? ";
+
+        try {
+
+            PreparedStatement preparedStatement = ConnectionJDBC.getConnectionJDBC().prepareStatement(sql);
+            preparedStatement.setString(1,name);
+            preparedStatement.setInt(2,id);
+            int rs = preparedStatement.executeUpdate();
+            logger.log(Level.INFO,String.valueOf(rs));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     public static void selectTable() {
         try {
-            String sql = "SELECT * from lop WHERE ID = 2";
+            String sql = "SELECT * from lop";
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()){
                 int id = resultSet.getInt("ID");
@@ -37,10 +56,19 @@ public class Main {
         }
     }
     public static void insertTable(int id,String name){
-        String sql = "INSERT INTO lop(ID,Name) values(2,'Quang')";
+        String sql = "INSERT INTO lop(ID,Name) values("+id+",'"+name+"')";
         try {
            int rs = statement.executeUpdate(sql);
            logger.log(Level.INFO,String.valueOf(rs));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void deleteTable(int id) {
+        String sql ="DELETE from lop where ID = "+id;
+        try {
+            int rs = statement.executeUpdate(sql);
+            logger.log(Level.INFO,String.valueOf(rs));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
